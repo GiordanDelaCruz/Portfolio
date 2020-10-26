eb5eed469d5c05d1551154bfdaec7802014d0a83
GiordanDelaCruz <GiordanDelaCruz@gmail.com>
2020-10-26 19:26:08 -0400

    Fixed spelling errors

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  // jQuery methods go here...
+  $(".main-header").hover(function(){
+    $(".main-header").addClass("bigger-main-header-text");
+  });
+
+  $(".main-header").hover(function(){
+    $(".main-header").addClass("bigger-main-header-text");
+  });
+
+  $(".main-header").removeClass("bigger-main-header-text");
+});
